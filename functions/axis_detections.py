Fix centoid calculation and title detection

functions/axis_detections.py
@@ -463,8 +463,8 @@ def add_int_to_tick_data(numbers_str_array, numbers_data):
         x2 = number[0] + number[2] + x_bias
         y2 = number[1] + number[3] + y_bias
         cv2.putText(edits.resized_gray, str(numbers_str_array[j]), (x2 - x_bias, y2), cv2.QT_FONT_NORMAL, 1, 0, 2)
-        centoid_x = np.round(number[0] + number[2] // 2)
-        centoid_y = np.round(number[1] + number[3] // 2)
+        centoid_x = np.round(number[0] + (number[2] // 2))
+        centoid_y = np.round(number[1] + (number[3] // 2))
         new_number = {
             "x": number[0],
             "y": number[1],
@@ -599,7 +599,7 @@ def find_values_of_simple_bars(ticks_data, bar_data, horizontal):
     bar_w_h_name = "h" if horizontal else "w"
 
     for i, bar in enumerate(bar_data):
-        bar_centoid = (bar[bar_x_y_name] + bar[bar_w_h_name]) // 2
+        bar_centoid = bar[bar_x_y_name] + (bar[bar_w_h_name] // 2)
 
         # Find the closest tick
         for tick in ticks_data:

functions/image_detections.py
@@ -27,7 +27,6 @@ resized_gray = None
 binary = None
 legend_orig = None
 legend_without_bars = None
-average_character_height = None
 global_column_ticks_str = []
 global_row_ticks_str = []
 column_numbers_data = []
@@ -39,7 +38,7 @@ colors = None
 def connected_components():
     global orientation, binary, num_size, percent, resized_gray, chart_with_bars_img, r_numbers, centoids, stats, max_y, new_numbers, \
         chart_title, elements, x_bias, y_bias, col_nums, row_nums, \
-        row_ticks_without_outliers, sorted_column_data, c_numbers_str, r_numbers_str, labels, average_character_height, global_row_ticks_str, global_column_ticks_str, row_numbers_data, column_numbers_data
+        row_ticks_without_outliers, sorted_column_data, c_numbers_str, r_numbers_str, labels, global_row_ticks_str, global_column_ticks_str, row_numbers_data, column_numbers_data
 
     binary = edits.threshold()
 
@@ -171,12 +170,14 @@ def connected_components():
 
 
 def detect_title(has_title):
+    title_area = int(axis_detections.average_character_height * 3)
+
     if has_title == 0:
         return None
     elif has_title == 1:
-        title_img = binary[0:200, 0:]
+        title_img = binary[0:title_area, 0:]
     elif has_title == -1:
-        title_img = binary[-200:, 0:]
+        title_img = binary[-title_area:, 0:]
 
     title_img = 255 - title_img
     chart_title = pytesseract.pytesseract.image_to_string(title_img, config=config_title)
@@ -294,9 +295,6 @@ def define_simple_chart_values(bars_with_colors):
         ratios = np.round(bar_hs / longest_bar["w"], 2)
         axis_detections.tick_number_of_longest_bar = axis_detections.find_value_of_bar(row_numbers_data, longest_bar,
                                                                                        True)
-
-        # if orientation == 'xbar':
-        #     ratios = np.flip(ratios)
 
         bar_x_values = ratios * axis_detections.tick_number_of_longest_bar
         bars_with_colors = axis_detections.find_values_of_simple_bars(column_numbers_data, bars_with_colors, True)